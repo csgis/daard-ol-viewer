605186fdadef46e2fe69f00b403dc423486fb405
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2024-01-25 19:05:44 +0100

    new build

--- docs/assets/afterMarkupFinish-1dbd0c94.js
+++
@@ -1,2 +0,0 @@
-import{m as e}from"./index-22092f9a.js";import{f as i}from"./feather-becb2378.js";const n=()=>{document.addEventListener("allPluginsFinishedLoading",()=>{console.log("- init enableTooltips"),i.replace()}),e.store("pluginStatus").increasePluginLoadingStatus()};export{n as initialize};
-//# sourceMappingURL=afterMarkupFinish-1dbd0c94.js.map
