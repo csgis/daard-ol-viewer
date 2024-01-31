eaf062490f301bbf89ac48b764fe6de7dae2780a
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2024-01-31 16:09:34 +0100

    new build

---
+++ docs/assets/afterMarkupFinish-77c6b399.js
@@ -0,0 +1,2 @@
+import{m as e}from"./index-2cd4fb54.js";import{f as i}from"./feather-becb2378.js";const n=()=>{document.addEventListener("allPluginsFinishedLoading",()=>{console.log("- init enableTooltips"),i.replace()}),e.store("pluginStatus").increasePluginLoadingStatus()};export{n as initialize};
+//# sourceMappingURL=afterMarkupFinish-77c6b399.js.map
--- docs/assets/afterMarkupFinish-b63584ed.js
+++
@@ -1,2 +0,0 @@
-import{m as e}from"./index-732974ed.js";import{f as i}from"./feather-becb2378.js";const n=()=>{document.addEventListener("allPluginsFinishedLoading",()=>{console.log("- init enableTooltips"),i.replace()}),e.store("pluginStatus").increasePluginLoadingStatus()};export{n as initialize};
-//# sourceMappingURL=afterMarkupFinish-b63584ed.js.map
