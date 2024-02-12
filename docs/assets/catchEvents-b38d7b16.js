0ec23c28beaf46122392d0a700ec8e2ebf4cdeaa
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2024-02-12 19:37:00 +0100

    new build

---
+++ docs/assets/catchEvents-b38d7b16.js
@@ -0,0 +1,2 @@
+import{m as t}from"./index-23d7eb7e.js";const i=()=>{document.addEventListener("filterPushed",function(e){console.debug('Custom event "filterPushed" caught in debug/catchEvents.js'),console.debug(e.detail.instance),console.debug(e.detail.instance.getUrl()),console.debug(e.detail.instance.getParams().LAYERS)}),t.store("pluginStatus").increasePluginLoadingStatus()};export{i as initialize};
+//# sourceMappingURL=catchEvents-b38d7b16.js.map
