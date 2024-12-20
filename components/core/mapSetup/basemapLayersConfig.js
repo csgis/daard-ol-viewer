7d0ad050cff100b3abd7cbeeafbd556c208d0734
Toni <toni.schoenbuchner@csgis.de>
2024-12-20 14:51:14 +0100

    fix base layer

--- components/core/mapSetup/basemapLayersConfig.js
+++ components/core/mapSetup/basemapLayersConfig.js
@@ -17,7 +17,7 @@ const basemapLayers = [
     },
     {
       "id": "osm-france",
-      "url": "https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+      "url": "https://tile.openstreetmap.bzh/eu/{z}/{x}/{y}.png",
       "attribution": "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
       "visible": false,
       "img": "/assets/img/basemaps/osm-hot-fr.jpeg"
